a1754a20e468692576b6545d46a44d5310ec586c
Christoph Hartmann <chris@lollyrock.com>
2013-10-21 16:08:16 +0200

    auto-install devstack

--- devstack.sh
+++ devstack.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# environment variables
+BRANCH=stable/havana
+
 # update system
 export DEBIAN_FRONTEND noninteractive
 sudo apt-get update || sudo yum update -y
@@ -11,8 +14,11 @@ DEVSTACK=$PWD/devstack
 echo Download devstack into $DEVSTACK
 
 # clone devstack
-BRANCH=stable/havana
 su vagrant -c "cd && git clone -b $BRANCH https://github.com/openstack-dev/devstack.git $DEVSTACK"
 
+# copy localrc settings (source: devstack/samples/localrc)
 cp /vagrant/config/localrc $DEVSTACK/localrc
 chown vagrant:vagrant $DEVSTACK/localrc
+
+# initialize devstack
+su vagrant -c "cd $DEVSTACK && ./stack.sh"
